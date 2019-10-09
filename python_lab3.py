7facffa5218877ca23eb0ca27ebba8148ad37ae6
SolomiyaKubinska <50668329+SolomiyaKubinska@users.noreply.github.com>
2019-10-09 18:14:44 +0300

    lab3. Decorate text with HTML tags

--- python_lab3.py
+++ python_lab3.py
@@ -1,25 +1,16 @@
-def decorator_for_html_tags(f):
-    def wrap(*args):
-        text = f(*args)
-        tags = {"paragraph": "p", "header": "h1", "hyperlink": "a", "article": "article", "body": "body", "button": "button", "column": "col", "directory list": "dir", "section": "div", "footer": "footer", "image": "img", "list item": "li", "metadata": "meta", "navigation links": "nav", "short quotation": "q", "script": "script"}
-        for x in tags.keys():
-            if text[0] == x:
-                print("<" + tags[x] + "> " + text[1] + " </" + tags[x] + ">")
-            elif text[0] == x:
-                print("<" + tags[x] + "> " + text[1] + " </" + tags[x] + ">")
-    return wrap
+def decorator_for_html_tags(tags=""):
+    def decorator(func):
+        def wrap(string):
+            print("<" + tags + "> " + str(func(string)) + " </" + tags + ">")
+        return wrap
+    return decorator
 
+@decorator_for_html_tags("div")
+def list(string):
+    return string
 
-@decorator_for_html_tags
-def list(*args):
-    return args
+def main():
+    return list(input("Enter text for html tags: "))
 
-
-m = input("Pick the html tag from (paragraph, header, hyperlink, "
-          "article, body, button, "
-          "column, directory list, section, "
-          "footer, image, list item, metadata,"
-          " navigation links, short quotation, script): ")
-o = input("Enter text fot html tags: ")
-
-list(m, o)
+if __name__ == '__main__':
+    main()
