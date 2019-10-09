5ee18d602ab3b979cf56fb17c05e5a2e12ca2b96
SolomiyaKubinska <50668329+SolomiyaKubinska@users.noreply.github.com>
2019-10-09 18:25:40 +0300

    lab1. Random list

--- lab1_python.py
+++ lab1_python.py
@@ -1,9 +1,14 @@
 import random
-print("Enter range: ")
-x = int(input())
-m = list(random.randint(0, 100) for i in range(x))
-print(m)
-s = sum(m)
-print(s)
-l = s/len(m)
-print(l)
+
+def main():
+    print("Enter range: ")
+    x = int(input())
+    m = list(random.randint(0, 100) for i in range(x))
+    print(m)
+    s = sum(m)
+    print(s)
+    l = s/len(m)
+    print(l)
+
+if __name__ == '__main__':
+    main()
