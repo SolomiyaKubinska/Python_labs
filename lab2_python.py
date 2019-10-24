7acf2820125f1c8df62050f182d982495758e4ca
lsbmy <50499203+lsbmy@users.noreply.github.com>
2019-10-24 23:03:42 +0300

    Update lab2_python.py

--- lab2_python.py
+++ lab2_python.py
@@ -4,19 +4,19 @@ def decorator(html_tags_remover):
         dict_of_tags = {"<": "&lt;", ">": "&gt;", "&": "&amp;", "\"": "&quot;"}
         for x in html_tags_remover(string):
             if x in dict_of_tags.keys():
-                x = dict_of_tags[x]
-                a.append(x)
+                a.append(dict_of_tags[x])
             else:
                 a.append(x)
         return print("".join(a))
     return wrap
 
 @decorator
-def printline1(string):
+def line(string):
     return string
 
 def main():
-    return printline1(input("Enter text for escaping html tags: "))
+    a = input("Enter text for escaping html tags: ")
+    line(a)
 
 if __name__ == '__main__':
     main()
