56a0db74c83f979b18c6d76fc423bf0e185fca42
lsbmy <50499203+lsbmy@users.noreply.github.com>
2019-10-24 23:17:37 +0300

    Update python_lab4.py

--- python_lab4.py
+++ python_lab4.py
@@ -1,4 +1,11 @@
-from functools import wraps
+def wraps(func):
+    def wrap_decorator(func0):
+        def html_wrap(string2):
+            return 0
+        html_wrap.__name__ = func.__name__
+        html_wrap.__doc__ = func.__doc__
+        return html_wrap
+    return wrap_decorator
 
 def html_decorator(func):
     @wraps(func)
@@ -15,7 +22,7 @@ def main():
     html_text(input("Enter text for html file: "))
     print(html_text.__name__)
     print(html_text.__doc__)
-
+    print(help(html_text))
 
 if __name__ == '__main__':
     main()
