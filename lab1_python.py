80599932ed127f5d3930ffc4af5eee8d0aaa186b
lsbmy <50499203+lsbmy@users.noreply.github.com>
2019-10-24 23:29:21 +0300

    lab1. random list

--- lab1_python.py
+++ lab1_python.py
@@ -1,14 +1,19 @@
 import random
 
+def lst(n, a, b):
+    lst0 = []
+    for i in range(n):
+        lst0.append(random.randint(a, b))
+    return lst0
+
 def main():
-    print("Enter range: ")
-    x = int(input())
-    m = list(random.randint(0, 100) for i in range(x))
-    print(m)
-    s = sum(m)
-    print(s)
-    l = s/len(m)
-    print(l)
+    n = int(input("Enter number: "))
+    a = int(input("Enter the beginning: "))
+    b = int(input("Enter the ending: "))
+    l = lst(n, a, b)
+    print("List - " + str(l))
+    print("Sum of the list - " + str(sum(l)))
+    print("Average of the list - " + str(sum(l)/n))
 
 if __name__ == '__main__':
     main()
