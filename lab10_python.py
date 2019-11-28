7e73f4f5aaec85e2767a7d0319e78c8164c141ca
lsbmy <50499203+lsbmy@users.noreply.github.com>
2019-11-28 20:16:12 +0200

    Lab10. Multi-threaded file download

--- lab10_python.py
+++ lab10_python.py
@@ -2,7 +2,26 @@ import os
 from urllib import request
 from threading import Thread
 
-class download_file(Thread):
+class download_file_1(Thread):
+    def __init__(self, url, num):
+        Thread.__init__(self)
+        self.num = num
+        self.url = url
+
+    def run(self):
+        file = request.urlopen(self.url)
+        path = os.path.basename(self.url)
+        with open(path, "wb") as f:
+            CHUNK = 1024 * 512
+            while True:
+                file_text = file.read(CHUNK)
+                if not file_text:
+                    break
+                f.write(file_text)
+        result = f"{self.num}. {self.url} is downloaded!"
+        print(result)
+
+class download_file_2(Thread):
     def __init__(self, url, num):
         Thread.__init__(self)
         self.num = num
@@ -22,11 +41,12 @@ class download_file(Thread):
         print(result)
 
 def main(urls0):
-    for num, url in enumerate(urls0):
-        thread = download_file(url, num+1)
-        thread.start()
+    thread_1 = download_file_1(urls0, 1)
+    thread_1.start()
+    thread_2 = download_file_2(urls0, 2)
+    thread_2.start()
 
 
 if __name__ == "__main__":
-    urls = input("Enter some url files: ").split(' ')
+    urls = str(input("Enter url file: "))
     main(urls)
